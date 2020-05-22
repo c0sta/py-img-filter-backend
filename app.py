19ab5cba9d529093e69fd6b2b72ece146382b201
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-22 20:00:35 -0300

    :tada: uploading initial files

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "AAAAAAAA"
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    # shows form
+    return '''  
+        <form> 
+            <input type="text" name="username" />
+            <input type="password" name="password" />
+            <button type="submit">Submit</button>
+        </form>
+    '''
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
