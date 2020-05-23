41732e17c3bcdd17e10ba201604a156ec6bcf606
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-23 02:59:09 -0300

    :tada: commiting basic files

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for, session
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    # shows form
-    return '''  
-        <form> 
-            <input type="text" name="username" />
-            <input type="password" name="password" />
-            <button type="submit">Submit</button>
-        </form>
-    '''
+@app.route('/')
+def hello_world():
+    return 'Hello, world!'
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    print('in the upload_file!')
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully!'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
