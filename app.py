aca3e88e004381b85e87b7cd11adf005909dd2cd
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-25 14:04:26 -0300

    :art: Added basic file upload resource

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, flash, request, redirect, url_for, session
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from google.colab.patches import cv2_imshow
+%matplotlib inline
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -14,7 +21,8 @@ def upload_file():
     print('in the upload_file!')
     if request.method == 'POST':
         f = request.files['file']
-        f.save(secure_filename(f.filename))
+        print('INFERNOOO', request.files)
+        f.save('./images/'+f.filename)
         return 'file uploaded successfully!'
 
 
